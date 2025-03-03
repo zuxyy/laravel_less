dfec9d786492f27ccaa1f0f22d7be10b1a740016
Zuxriddin <xolixmatovzuxriddin@gmail.com>
2025-03-03 14:48:51 +0500

    jwt toke

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/posts', [PostController::class, 'store']);
+Route::group([
 
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('me', [App\Http\Controllers\AuthController::class, 'me']);
+
+});
+
+Route::middleware('jwt.auth')->prefix('posts')->name('posts.')->group(function () {
+//    Route::get('/', 'index');
+    Route::get('/', [PostController::class, 'index'])->name('index');
+});
+# WITH MIDDLEWARE
+//Route::controller(PostController::class)->prefix('posts')->group(function () {
+//});
